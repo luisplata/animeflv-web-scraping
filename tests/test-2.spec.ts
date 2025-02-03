482b62001807217b15e60de877ab0588ad32de49
PeryLoth <www.luisplata@gmail.com>
2025-02-03 18:09:14 -0500

    remove trash files

--- tests/test-2.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www3.animeflv.net/');
-  await page.getByRole('link', { name: 'Watashi no Shiawase na' }).click();
-  await page.getByRole('link', { name: 'Opción 1' }).click();
-  await page.locator('#video_box iframe').contentFrame().locator('#video').click();
-});
