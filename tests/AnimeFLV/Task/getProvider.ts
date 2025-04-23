770fe55b67d793b19bad022c893697d3b3a35b33
PeryLoth <www.luisplata@gmail.com>
2025-04-22 20:44:01 -0500

    log to what page is loading

--- tests/AnimeFLV/Task/getProvider.ts
+++ tests/AnimeFLV/Task/getProvider.ts
@@ -2,7 +2,7 @@ import {SpecificCap} from "../Page/SpecificCap";
 import {Episode, Source} from "../Data/json";
 
 export const getProvider = () => async (cap: Episode, page: SpecificCap) => {
-    const unsupport = ["netu", "stape"];
+    const unsupport = ["netu", "stape", "Fembed"];
     const capPage = await page.getListOfOptions();
     for (let option of capPage) {
         let titleOption = await page.getTitleOfOption(option);
@@ -10,7 +10,7 @@ export const getProvider = () => async (cap: Episode, page: SpecificCap) => {
             continue;
         }
         //log the option text from Locator
-        console.log("🔗 Cap provider::", await option.textContent());
+        console.log("🔗 Cap provider::", await option.textContent(), cap.number);
         let videoSource = await page.getLinkToView(option);
         console.log("🔗 Cap to get provider::", cap.title, cap.number, titleOption, videoSource);
         let source = new Source(titleOption, videoSource);
